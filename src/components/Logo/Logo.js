be2b0c17f4580705128b536adb8774377819eb63
RecodeExistence <recodeexistance@gmail.com>
2020-01-23 16:33:21 +0000

    created logo, sorted positioning.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './Logo.css'
-
+import LogoPng from './logo.png'
 const Logo = () => {
     return (
         <div className = "Logo">
             {/*Placeholder till logo created */}
-            <img src = "https://picsum.photos/536/354" alt = "KitchenPal Logo" />
+            <img src = {LogoPng} alt = "KitchenPal Logo" />
         </div>
     );
 }
